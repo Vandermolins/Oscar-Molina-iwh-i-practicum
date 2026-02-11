f30381ea8d073b33b7aa840141f35694bf46bcb1
Vandermolins <vandermolins@gmail.com>
2026-02-11 15:49:47 -0600

    Update index.js
    updated

--- index.js
+++ index.js
@@ -7,65 +7,119 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
+// * Please DO NOT INCLUDE the private app access token in your repo.
 const PRIVATE_APP_ACCESS = '';
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+// =====================================================
+// ROUTE 1 - Homepage (using your Postman JSON response)
+// =====================================================
+app.get('/', (req, res) => {
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
+    // Simulated HubSpot response (based on your Postman result)
+    const hubspotResponse = {
+        results: [
+            {
+                id: "201478040975",
+                properties: {
+                    createdate: "2026-02-11T20:15:05.647Z",
+                    email: "superuser@nintendo.com",
+                    firstname: "Shigeru",
+                    hs_object_id: "201478040975",
+                    lastmodifieddate: "2026-02-11T20:15:17.662Z",
+                    lastname: "Miyamoto"
+                },
+                createdAt: "2026-02-11T20:15:05.647Z",
+                updatedAt: "2026-02-11T20:15:17.662Z",
+                archived: false,
+                url: "https://app.hubspot.com/contacts/50966491/record/0-1/201478040975"
+            },
+            {
+                id: "201478096564",
+                properties: {
+                    createdate: "2026-02-11T19:22:23.241Z",
+                    email: "demand@nintendo.com",
+                    firstname: "Mario ",
+                    hs_object_id: "201478096564",
+                    lastmodifieddate: "2026-02-11T19:35:26.741Z",
+                    lastname: "Castaneda"
+                },
+                createdAt: "2026-02-11T19:22:23.241Z",
+                updatedAt: "2026-02-11T19:35:26.741Z",
+                archived: false,
+                url: "https://app.hubspot.com/contacts/50966491/record/0-1/201478096564"
+            },
+            {
+                id: "201504513327",
+                properties: {
+                    createdate: "2026-02-11T20:16:22.471Z",
+                    email: "redaction@xbox.com",
+                    firstname: "Phill ",
+                    hs_object_id: "201504513327",
+                    lastmodifieddate: "2026-02-11T20:16:45.067Z",
+                    lastname: "Spencer"
+                },
+                createdAt: "2026-02-11T20:16:22.471Z",
+                updatedAt: "2026-02-11T20:16:45.067Z",
+                archived: false,
+                url: "https://app.hubspot.com/contacts/50966491/record/0-1/201504513327"
+            }
+        ]
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
+    // Transform data for front-end
+    const data = hubspotResponse.results.map(contact => ({
+        id: contact.id,
+        fullName: `${contact.properties.firstname.trim()} ${contact.properties.lastname.trim()}`,
+        email: contact.properties.email,
+        createdAt: contact.createdAt,
+        updatedAt: contact.updatedAt,
+        url: contact.url
+    }));
 
+    res.render('homepage', {
+        title: 'Contacts | HubSpot APIs',
+        data
+    });
 });
-*/
 
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+// =====================================================
+// ROUTE 2 - Render Form
+// =====================================================
+app.get('/form', (req, res) => {
+    res.render('form', {
+        title: 'Create or Update Contact'
+    });
+});
+
+
+// =====================================================
+// ROUTE 3 - Simulated Create / Update
+// =====================================================
+app.post('/form', (req, res) => {
+
+    const newContact = {
+        properties: {
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            email: req.body.email
+        }
+    };
+
+    console.log('New contact received from form:');
+    console.log(newContact);
+
+    // Aquí iría axios.post(...) si fuera real
+    // Pero estamos usando datos simulados
+
+    res.redirect('/');
+});
+
+
+// =====================================================
+// Localhost
+// =====================================================
+app.listen(3000, () => 
+    console.log('Listening on http://localhost:3000')
+);
